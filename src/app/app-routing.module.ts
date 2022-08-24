2ec27f03bd54f990dd55aa93cc66925aef026ca4
Python The Hack <lrulvoni@gmail.com>
2022-08-24 10:12:57 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ServiziComponent } from './pages/servizi/servizi.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+    { path: 'servizi-component', component: ServiziComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiziComponent } from './pages/servizi/servizi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServiziComponent
   ],
   imports: [
     BrowserModule,
