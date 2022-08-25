726f71b22113aacd09d3c6108f4e6f28efcc0433
Python The Hack <lrulvoni@gmail.com>
2022-08-25 17:23:16 +0200

    update pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatCardModule } from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiziComponent } from './pages/servizi/servizi.component';
+import { FooterComponent } from './footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServiziComponent
+    ServiziComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatCardModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/servizi/servizi.component.ts
+++ src/app/pages/servizi/servizi.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-servizi',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServiziComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+    //private route: ActivatedRoute; 
+   }
 
   ngOnInit(): void {
   }
